15c5878470e1a4cdfb184c0cb83ede1e1b2bad25
Max Proano <proanom89@gmail.com>
2021-08-02 17:16:11 -0700

    Connect to weather API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
+const axios = require('axios');
 
 app.use(express.static('public'));
 
+app.get('/weather', async (req, res) => {
+  try {
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=1689969&appid=${process.env.OPEN_WEATHER_KEY}`);
+    res.send(result.data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Weather app listening at http://localhost:${port}`);
 });
