e49e82a07cc9335b9bc14aca50b406969b5bba93
Max Proano <proanom89@gmail.com>
2021-08-03 13:58:51 -0700

    Create function to retrieve daily forecast

---
+++ server/controllers.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const { getWeatherData } = require('./apiFetcher.js');
+const URL = `https://api.openweathermap.org/data/2.5/onecall`
+
+module.exports.getDailyForecast = async (req, res) => {
+  const params = {
+    lat: 15.6287,
+    lon: 120.436798,
+    exclude: 'minutely,hourly,alerts',
+    appid: process.env.OPEN_WEATHER_KEY
+  }
+
+  getWeatherData(URL, params)
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+};
