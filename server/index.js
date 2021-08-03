95baef35eb467e02261eb652f50f3211f8f5b6be
Max Proano <proanom89@gmail.com>
2021-08-03 13:59:38 -0700

    Complete GET /dailyForecast

--- server/index.js
+++ server/index.js
@@ -3,17 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const axios = require('axios');
+const { getDailyForecast } = require('./controllers.js');
 
 app.use(express.static('public'));
 
-app.get('/weather', async (req, res) => {
-  try {
-    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=1689969&appid=${process.env.OPEN_WEATHER_KEY}`);
-    res.send(result.data);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.get('/dailyForecast', getDailyForecast);
 
 app.listen(port, () => {
   console.log(`Weather app listening at http://localhost:${port}`);
