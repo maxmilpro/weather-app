ee58154f41a0f6305cdc14fe05228586c3c0fc66
Max Proano <proanom89@gmail.com>
2021-08-04 18:31:25 -0700

    Format return value of getDailyForecast

--- server/controllers.js
+++ server/controllers.js
@@ -4,13 +4,34 @@ const URL = `https://api.openweathermap.org/data/2.5/onecall`
 
 module.exports.getDailyForecast = (req, res) => {
   const params = {
-    lat: 15.6287,
-    lon: 120.436798,
+    lat: 37.7749,
+    lon: -122.4194,
+    units: 'imperial',
     exclude: 'minutely,hourly,alerts',
     appid: process.env.OPEN_WEATHER_KEY
   }
 
   getWeatherData(URL, params)
-    .then(result => res.send(result))
+    .then(result => res.send(generateFiveDayForecast(result.daily)))
     .catch(err => res.send(err));
+};
+
+const generateFiveDayForecast = (data) => {
+  const forecasts = data.map((day) => {
+    const [ dayOfWeek, date ] = parseTimestamp(day.dt);
+
+    return {
+      dayOfWeek,
+      date,
+      temperature: day.temp.day,
+      description: day.weather[0].description
+    }
+  });
+
+  return forecasts.slice(0, 5);
+};
+
+const parseTimestamp = (timestamp) => {
+  const dateString = new Date(timestamp * 1000).toDateString();
+  return dateString.split(/(?<=^\S+)\s/);
 };
