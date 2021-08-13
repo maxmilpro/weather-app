a8881c1b5a3a222e96980e6c9ed1cfb1be9ffabe
Max Proano <proanom89@gmail.com>
2021-08-12 18:52:05 -0700

    Add weather icons

--- src/components/DayForecast.jsx
+++ src/components/DayForecast.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { CloudLightningRain, CloudDrizzle, CloudRain, CloudSnow, Sun, Cloud } from '@styled-icons/bootstrap';
 import { StyledDayForecast } from '../styles/StyledDayForecast.jsx';
+
+const iconMap = {
+  Thunderstorm: <CloudLightningRain size='48'/>,
+  Drizzle: <CloudDrizzle size='48'/>,
+  Rain: <CloudRain size='48'/>,
+  Snow: <CloudSnow size='48'/>,
+  Clear: <Sun size='48'/>,
+  Clouds: <Cloud size='48'/>
+}
 
 const DayForecast = ({ day }) => {
   return (
     <StyledDayForecast>
       <h2>{day.dayOfWeek}</h2>
       <h3>{day.date}</h3>
-      <h3>{day.temperature}</h3>
+      <h3>{Math.round(day.temperature)}°F</h3>
+      {iconMap[day.main]}
       <h3>{day.description}</h3>
     </StyledDayForecast>
   )
