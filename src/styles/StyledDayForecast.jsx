70f361025303d4215708a729ed60d4c1947303f0
Max Proano <proanom89@gmail.com>
2021-08-12 18:52:36 -0700

    Add border and margin

--- src/styles/StyledDayForecast.jsx
+++ src/styles/StyledDayForecast.jsx
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 
 const StyledDayForecast = styled.div`
   text-align: center;
+  border-radius: 25px;
+  border: 2px solid black;
+  width: 200px;
+  margin: auto;
+  margin-bottom: 5px;
+  background-color: gainsboro;
 `;
 
 export { StyledDayForecast };
